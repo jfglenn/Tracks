5b2bfb233df8e362adc25cd9e940978d6090ee98
jfglenn <jessieglenn2002@gmail.com>
2024-09-10 23:14:09 -0500

    adding login related pages

---
+++ bpm/src/views/BounceView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="callback">
+    <p>Hang in there, redirecting...</p>
+  </div>
+</template>
+
+<script>
+<style scoped>
+.callback {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 18px;
+}
+</style>
---
+++ bpm/src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Spotify Login Example</h1>
+    <button @click="login">Login with Spotify</button>
+  </div>
+</template>
+
+<script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
